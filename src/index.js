cd71fce09f74745965a235e4ae0eebd56a76d540
Tahin Rahman <tahin.36@gmail.com>
2019-02-16 11:04:48 +0600

    index.js cleaned

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import adhan from "adhan";
 import Calendar from "./components/Calendar/Calendar.js";
 import Location from "./components/Location/Location.js";
 import PrayerTimeTable from "./components/PrayerTimeTable/PrayerTimeTable.js";
 import CoordsContext from "./CoordsContext";
 
 import "./styles.css";
-//https://cdn.dribbble.com/users/981797/screenshots/2743209/iman-time.jpg
-//https://github.com/batoulapps/adhan-js
-//https://najens.github.io/weather-icons-react/
 
 function App() {
   const [coords, setCoords] = useState({
