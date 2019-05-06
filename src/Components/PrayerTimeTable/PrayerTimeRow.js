a2edaab0a31793cceca82f92cbeaef84999260db
Tahin Rahman <tahin.36@gmail.com>
2019-05-07 00:20:38 +0600

    prayertimetable component issue fixed

---
+++ src/Components/PrayerTimeTable/PrayerTimeRow.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function PrayerTimeRow(props) {
+  return (
+    <li
+      id="prayerTimeRow"
+      className={props.isCurrent ? "current-prayer-time" : ""}
+    >
+      <div className="prayer-icon">{props.icon}</div>
+      <div className="prayer-name">{props.name}</div>
+      <div className="prayer-time">{props.formattedTime}</div>
+      {props.isCurrent ? (
+        <div className="prayer-time-remaining">
+          Time until {props.next}: {props.timeUntilNextPrayerInText}
+        </div>
+      ) : null}
+    </li>
+  );
+}
+
+export default PrayerTimeRow;
---
+++ src/Components/PrayerTimeTable/PrayerTimeTable.js
@@ -0,0 +1,97 @@
+import React, { useState, useContext, useEffect } from "react";
+import PrayerTimeRow from "./PrayerTimeRow.js";
+import PrayerTimeService from "../../Services/prayertime.service.js";
+import {
+  WiDayHaze,
+  WiDayCloudy,
+  WiDaySunny,
+  WiDayCloudyHigh,
+  WiNightCloudy,
+  WiNightAltPartlyCloudy,
+  WiNightClear
+} from "weather-icons-react";
+import { SettingsContext } from "../../Contexts/settings.context";
+import AppConfig from "../../AppConfig.json";
+
+import "./PrayerTimeTable.css";
+
+function PrayerTimeTable(props) {
+  if (!props.latitude || !props.longitude) return null;
+
+  const settingsContext = useContext(SettingsContext);
+  const getUpdatedPrayerTimes = getPrayerTimesToDisplay.bind(this, props.latitude, props.longitude,
+    settingsContext.selectedOptions.method.optionKey, settingsContext.selectedOptions.madhab.optionKey);
+  const [prayerTimes, setPrayerTimes] = useState(
+    getUpdatedPrayerTimes()
+  );
+
+  useEffect(() => {
+    const [prayerTimeTimeout, prayerTimeInterval] =
+      setupUpdatePrayerTimeInEveryMinute(getUpdatedPrayerTimes, setPrayerTimes);
+
+    return () => {
+      clearTimeout(prayerTimeTimeout);
+      clearInterval(prayerTimeInterval);
+    };
+  });
+
+  return (
+    <ul id="prayerTimeTable">
+      {prayerTimes.map((prayerTime, index) => (
+        <PrayerTimeRow key={index} {...prayerTime} />
+      ))}
+    </ul>
+  );
+}
+
+function setupUpdatePrayerTimeInEveryMinute(getUpdatedPrayerTimes, setPrayerTimes) {
+  const now = new Date();
+  const SECONDS_REMAINING_TILL_THIS_MINUTE = (60 - now.getSeconds()) * 1000;
+  const ONE_SECOND = 60 * 1000;
+
+  let prayerTimeTimeout, prayerTimeInterval;
+  prayerTimeTimeout = setTimeout(() => {
+    setPrayerTimes(getUpdatedPrayerTimes());
+
+    prayerTimeInterval = setInterval(() => {
+      setPrayerTimes(getUpdatedPrayerTimes());
+    }, ONE_SECOND);
+  }, SECONDS_REMAINING_TILL_THIS_MINUTE);
+
+  return [prayerTimeTimeout, prayerTimeInterval];
+}
+
+function getPrayerTimesToDisplay(latitude, longitude, method, madhab) {
+  const prayerTimeService = new PrayerTimeService(latitude, longitude, method, madhab);
+  let prayerTimes = prayerTimeService.getPrayerTimes();
+  let prayerTimesToDisplay = AppConfig.prayerTimesToShow.map(timeKey => {
+    let prayerTime = prayerTimes[timeKey];
+    prayerTime.icon = getPrayerTimeIcon(timeKey, 38);
+    return prayerTimes[timeKey];
+  });
+
+  return prayerTimesToDisplay;
+}
+
+function getPrayerTimeIcon(prayerTimeKey, size) {
+  switch (prayerTimeKey) {
+    case "fajr":
+      return <WiDayHaze size={size} />;
+    case "sunrise":
+      return <WiDayCloudy size={size} />;
+    case "dhuhr":
+      return <WiDaySunny size={size} />;
+    case "asr":
+      return <WiDayCloudyHigh size={size} />;
+    case "maghrib":
+      return <WiNightCloudy size={size} />;
+    case "isha":
+      return <WiNightAltPartlyCloudy size={size} />;
+    case "midnight":
+      return <WiNightClear size={size} />;
+    default:
+      return <WiDaySunny size={size} />;
+  }
+}
+
+export default PrayerTimeTable;
